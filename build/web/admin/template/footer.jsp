41e55dc3c99a9cba76ac02853526770ba155ff73
Abhishek Peiris <abhishekpeiris9@gmail.com>
2021-05-23 21:09:19 +0530

    initial commit

--- build/web/admin/template/footer.jsp
+++
@@ -1,8 +0,0 @@
-<%-- 
-    Document   : footer
-    Created on : May 12, 2021, 8:42:07 PM
-    Author     : abhi
---%>
-
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<footer class="footer">© 2021 Admin - Cyber Crime</footer>
--- src/java/mail/Mail.java
+++ src/java/mail/Mail.java
@@ -19,9 +19,10 @@ public class Mail {
 
     public static void sendMail(String to, String subject, String msg) {
 
-
-        final String username = "";
-        final String password = "";
+//        final String username = "abhitest999@gmail.com";
+//        final String password = "mB6Ph5SK5jbJww2";
+        final String username = "lkpet.team@gmail.com";
+        final String password = "2001adp54";
 
         // gmail mail server confi start
         Properties prop = new Properties();
